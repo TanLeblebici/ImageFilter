c88d2db1a22cb5a286f6f4c12632627d2a40e0d6
TanLeblebici <75142903+TanLeblebici@users.noreply.github.com>
2022-09-16 11:43:02 +0300

    Add files via upload

---
+++ InstafilterA/InstafilterA/InstafilterAApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterAApp.swift
+//  InstafilterA
+//
+//  Created by Tan Leblebici on 8.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
